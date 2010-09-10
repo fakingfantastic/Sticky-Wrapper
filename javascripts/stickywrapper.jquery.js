4fee448879d54a680be5fa8424aae42478dd08f9
Floyd Price <f.price@componentworkshop.com>
2010-09-10 17:43:42 +0100

    Removed refs to $ and renamed the trakcer funtion

--- javascripts/stickywrapper.jquery.js
+++ javascripts/stickywrapper.jquery.js
@@ -1,31 +1,32 @@
-(function($){  
-	$.fn.extend({   
+(function(jQuery){  
+	jQuery.fn.extend({   
 
 		stickyWrapper: function() {  
 			
-			$(this).parent().prepend('<div class="sticky-wrapper"></div>');
+			jQuery(this).parent().prepend('<div class="sticky-wrapper"></div>');
 
-			function tracker(elm) {
-				elmTopOffset = $(elm).offset().top
+			function positionStickyHeader(elm) {
+				elmTopOffset = jQuery(elm).offset().top
 				vOffset = (document.documentElement.scrollTop || document.body.scrollTop) - elmTopOffset;
 
 				if(vOffset > 0) {
-					$(elm).css("visibility", "hidden");  
-					$('.sticky-wrapper').show();
+					jQuery(elm).css("visibility", "hidden");  
+					jQuery('.sticky-wrapper').show();
 				} else {
-					$(elm).css("visibility", "visible");
-					$('.sticky-wrapper').hide();    
+					jQuery(elm).css("visibility", "visible");
+					jQuery('.sticky-wrapper').hide();    
 				}
 			}
 
 			return this.each(function() { 
-				var $elm = this; 
-				$(window).scroll(function(){
-						tracker($elm);
+				var jQueryelm = this; 
+				jQuery(window).scroll(function(){
+					positionStickyHeader(jQueryelm);
 				});
-				var $clone = $(this).clone(true).width($(this).width());
-				$('.sticky-wrapper').append($clone);
-				$(".sticky-wrapper").height($(".sticky-wrapper").height() + 15); // Add in height of BG Fade
+				var jQueryclone = jQuery(this).clone(true).width(jQuery(this).width());
+				jQuery('.sticky-wrapper').append(jQueryclone);
+				jQuery(".sticky-wrapper").height(jQuery(".sticky-wrapper").height() + 15); // Add in height of BG Fade
+				positionStickyHeader(jQueryelm);
 			}); 
 
 		}  
